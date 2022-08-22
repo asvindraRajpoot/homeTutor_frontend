f0dc21badc0ef2dcd5be214bf3a3df6c9d7b4d4d
Asvindra Rajpoot <aswin1399904@gmail.com>
2022-08-22 13:52:29 +0530

    Addec colors

---
+++ src/variables.ts
@@ -0,0 +1,5 @@
+export  const primaryColor='#F7AC08';
+export  const secondaryColor='#062347';
+export  const textColor='#7B838A';
+export const cardBackgroundColor='#F9F9FF';
+export const fontFamily='Roboto';
